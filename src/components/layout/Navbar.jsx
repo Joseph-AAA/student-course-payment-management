08b5b27f8ccd4ad8d95509f8a10049d808d1a093
Joseph25 <zaithangmawiaus@gmail.com>
2026-09-05 00:16:52 +0800

    update sidebar and navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,14 @@
 function Navbar() {
-  return <div>
-                I am the navbar
-         </div>
+  return <>
+              <form className="flex justify-between items-center gap-4 container 
+                               h-full  ">
+                <input
+                   placeholder = "Search Students..."
+                       className="h-9 w-[45%] rounded-md border border-gray-300 text-sm 
+                                  focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-e-blue-300 px-[8px] "
+                  />
+              
+              </form>  
+         </>
 }
 export default Navbar;
