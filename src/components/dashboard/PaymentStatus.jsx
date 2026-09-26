8d77c7f9d10324e1a808babed8d6a2190bcf1413
Joseph25 <zaithangmawiaus@gmail.com>
2026-09-26 22:21:23 +0800

    update UI for Payment Status

--- src/components/dashboard/PaymentStatus.jsx
+++ src/components/dashboard/PaymentStatus.jsx
@@ -1,8 +1,8 @@
 import PaymentStatusPieChart from './PaymentStatusPieChart'
 function PaymentStatus(){
     return(
-        <div className="w-full bg-white min-h-96 rounded-xl flex justify-center items-center">
-            <div className="w-[90%] h-[90%] mt-2 rounded-2xl">
+        <div className="w-full bg-white h-72 rounded-xl flex justify-center p-2 items-center">
+            <div className="w-[90%]   rounded-2xl">
                     <PaymentStatusPieChart />
             </div>
         </div>
